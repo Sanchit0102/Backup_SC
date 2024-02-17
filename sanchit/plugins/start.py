7cf1586327238e623cdc2768d608b9cf26a6409e
Unstoppable Sanchit <137860157+Sanchit0102@users.noreply.github.com>
2024-02-17 17:57:34 +0530

    Update start.py

--- sanchit/plugins/start.py
+++ sanchit/plugins/start.py
@@ -8,8 +8,8 @@ from ..screenshotbot import ScreenShotBot
 @ScreenShotBot.on_message(Filters.private & Filters.command(start))
 async def start(c, m):
 
-    await m.reply_text()
-        text=Hi there {m.from_user.mention}. \n\nI'm Screenshot Generator Bot. I can provide screenshots from your video files without downloading the entire file (almost instantly). For more details check help.
+    await m.reply_text(
+        text=Hi there {m.from_user.mention}. \n\nI'm Screenshot Generator Bot. I can provide screenshots from your video files without downloading the entire file (almost instantly). For more details check help.)
         quote=True,
         reply_markup=InlineKeyboardMarkup(
             [
