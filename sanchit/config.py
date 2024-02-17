86720224789237f12a8f4c8adfe7fdae09baf286
Unstoppable Sanchit <137860157+Sanchit0102@users.noreply.github.com>
2024-02-17 17:15:16 +0530

    Update config.py

--- sanchit/config.py
+++ sanchit/config.py
@@ -4,15 +4,15 @@ from pathlib import Path
 
 class Config:
 
-    API_ID = int(os.environ.get('API_ID'))
-    API_HASH = os.environ.get('API_HASH')
-    BOT_TOKEN = os.environ.get('BOT_TOKEN')
+    API_ID = int(os.environ.get('API_ID', 25833520))
+    API_HASH = os.environ.get('API_HASH', '7d012a6cbfabc2d0436d7a09d8362af7')
+    BOT_TOKEN = os.environ.get('BOT_TOKEN', '6362281925:AAGSTs25eytppfuN-AF-IMKCtxrvtxhXWJ8')
     SESSION_NAME = os.environ.get('SESSION_NAME', ':memory:')
-    LOG_CHANNEL = int(os.environ.get('LOG_CHANNEL'))
-    DATABASE_URL = os.environ.get('DATABASE_URL')
-    AUTH_USERS = [int(i) for i in os.environ.get('AUTH_USERS', '').split(' ')]
-    MAX_PROCESSES_PER_USER = int(os.environ.get('MAX_PROCESSES_PER_USER', 2))
-    MAX_TRIM_DURATION = int(os.environ.get('MAX_TRIM_DURATION', 600))
+    LOG_CHANNEL = int(os.environ.get('LOG_CHANNEL', -1002016803498))
+    DATABASE_URL = os.environ.get('DATABASE_URL', 'mongodb+srv://trumbot:trumbot@cluster0.cfkaeno.mongodb.net/?retryWrites=true&w=majority')
+    AUTH_USERS = [int(i) for i in os.environ.get('AUTH_USERS', '563896360 974706111 921365334').split(' ')]
+    MAX_PROCESSES_PER_USER = int(os.environ.get('MAX_PROCESSES_PER_USER', 10))
+    MAX_TRIM_DURATION = int(os.environ.get('MAX_TRIM_DURATION', 6000))
     TRACK_CHANNEL = int(os.environ.get('TRACK_CHANNEL', False))
     SLOW_SPEED_DELAY = int(os.environ.get('SLOW_SPEED_DELAY', 5))
     HOST = os.environ.get('HOST', '')
